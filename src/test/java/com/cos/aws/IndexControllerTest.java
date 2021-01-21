f11d5d3c470d2d076b26f470f424d1049ddd421c
chmh0805 <chmh0805@gmail.com>
2021-01-21 15:34:51 +0900

    v1

---
+++ src/test/java/com/cos/aws/IndexControllerTest.java
@@ -0,0 +1,13 @@
+package com.cos.aws;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class IndexControllerTest {
+	
+	@Test
+	public void indexTest() {
+		assertEquals(2, 2);
+	}
+}
